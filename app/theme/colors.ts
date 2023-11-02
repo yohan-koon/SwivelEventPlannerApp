7fb63148d48bd54f90e760d80ee076be9fb53a41
Yohan Alahakoon <yohankoon@gmail.com>
2023-11-02 11:44:13 +0530

    Completed signup UI

--- app/theme/colors.ts
+++ app/theme/colors.ts
@@ -34,7 +34,7 @@ const palette = {
   accent500: "#FFBB50",
 
   angry100: "#F2D6CD",
-  angry500: "#C03403",
+  angry500: "#D2042D",
 
   overlay20: "rgba(25, 16, 21, 0.2)",
   overlay50: "rgba(25, 16, 21, 0.5)",
---
+++ app/validators/schemas/index.ts
@@ -0,0 +1,2 @@
+export {getLoginFormValidationSchema} from './loginFormValidationSchema';
+export {getSignUpFormValidationSchema} from './signUpFormValidationSchema';
